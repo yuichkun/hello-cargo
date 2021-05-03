e0eb10474b87cd40ef0a3217cf54aaab30028e03
Yuichi Yogo <yuichkun@gmail.com>
2021-05-04 04:24:23 +0900

    invalid lifetime parameter example

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
-
-struct Sensor {
-    active: bool,
-    latest: u32
+struct Image<'a> {
+    raw: &'a [u8; 256]
 }
-
-struct SensorFusion {
-    temperature: Sensor,
-    light: Sensor,
-}
-
 fn main() {
-    let sensors = SensorFusion {
-        temperature: Sensor {
-            active: false,
-            latest: 0
-        },
-        light:  Sensor {
-            active: true,
-            latest: 3
-        }
-    };
-    let _l = sensors.light;
-    // println!("sensors.light.latest {}", sensors.light.latest);
-    println!("_l.latest {}", _l.latest);
-    println!("sensors.temperature.latest {}", sensors.temperature.latest);
+    let image;
+    {
+        let bytes: [u8; 256] = [0; 256];
+        image = Image {
+            raw: &bytes
+        };
+    }
+    println!("raw {}", image.raw[0]);
 
 }
