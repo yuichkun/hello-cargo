dde7901a1465705724078000a8a775ac0c18b81e
Yuichi Yogo <yuichkun@gmail.com>
2021-05-04 03:49:42 +0900

    ownership example

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 
-#[derive(Debug)]
-enum Error {
-    Overflow,
-    Zero
+struct Sensor {
+    active: bool,
+    latest: u32
 }
 
-fn double(n: u32) -> Result<u32, Error> {
-    if n == 0 {
-        return Err(Error::Zero)
-    }
-    if n > (u32::MAX / 2) {
-        return Err(Error::Overflow)
-    }
-    Ok(n)
+struct SensorFusion {
+    temperature: Sensor,
+    light: Sensor,
 }
 
 fn main() {
-    let array = [1, u32::MAX, 0];
-
-    for n in array.iter() {
-        if let Ok(x) = double(*n - 2) {
-            println!("result with n {}, x {}", n, x);
+    let sensors = SensorFusion {
+        temperature: Sensor {
+            active: false,
+            latest: 0
+        },
+        light:  Sensor {
+            active: true,
+            latest: 3
         }
-    }
+    };
+    let _l = sensors.light;
+    // println!("sensors.light.latest {}", sensors.light.latest);
+    println!("_l.latest {}", _l.latest);
+    println!("sensors.temperature.latest {}", sensors.temperature.latest);
 
 }
