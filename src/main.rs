59aaf6bf4c24f86e39f25600ae352cbd966d90d2
Yuichi Yogo <yuichkun@gmail.com>
2021-05-04 03:06:27 +0900

    struct example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Sensor {
+    active: bool,
+    latest: u32,
+}
+
+impl Sensor {
+    fn new() -> Sensor {
+        Sensor {
+            active: false,
+            latest: 0
+        }
+    }
+
+    fn read(&self) -> u32 {
+        self.latest
+    }
+    fn init(&mut self) {
+        self.active = true;
+        self.latest = 42;
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut sensor = Sensor::new();
+    sensor.init();
+    println!("active = {}, latest ={}", sensor.active, sensor.read());
+    sensor.active = false;
+    sensor.latest = 32;
+    println!("active = {}, latest ={}", sensor.active, sensor.read());
 }
