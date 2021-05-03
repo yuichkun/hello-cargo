58f177dccba56d37fe72707ca11dd8389411d571
Yuichi Yogo <yuichkun@gmail.com>
2021-05-04 04:35:59 +0900

    trait exmpale

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
-struct Image<'a> {
-    raw: &'a [u8; 256]
+trait Sensor {
+    fn read(&self) -> u32;
 }
-fn main() {
-    let image;
-    {
-        let bytes: [u8; 256] = [0; 256];
-        image = Image {
-            raw: &bytes
-        };
+
+struct LightSensor {
+    value: u32,
+}
+
+impl Sensor for LightSensor {
+    fn read(&self) -> u32 {
+        self.value
+     }
+}
+
+struct TemperatureSensor {
+    value: f32
+}
+
+impl Sensor for TemperatureSensor {
+
+    fn read(&self) -> u32 {
+        self.value as u32
     }
-    println!("raw {}", image.raw[0]);
+}
+
+fn main() {
+    let ls = LightSensor {
+        value: 1
+    };
+    let ts = TemperatureSensor {
+        value: 3.5
+    };
+
+    let mut res: u32 = ls.read();
+    println!("res {}",res);
+    res = ts.read();
+    println!("res {}",res);
 
 }
